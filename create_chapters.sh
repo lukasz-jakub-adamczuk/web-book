5a4cd29a42652548e49db985da89df148406b642
Lukasz Adamczuk <lukasz.jakub.adamczuk@gmail.com>
2013-08-22 09:42:37 +0200

    cleaning mess

--- create_chapters.sh
+++ create_chapters.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
-cd src/chapters/html
-cat $(cat ../../../chpt-html.list) > ../../../tmp/html.md
+cd src/chapters/xhtml
+cat $(cat ../../../chpt-xhtml.list) > ../../../tmp/xhtml.md
+
+cd ../html5
+cat $(cat ../../../chpt-html5.list) > ../../../tmp/html5.md
 
 cd ../css
 cat $(cat ../../../chpt-css.list) > ../../../tmp/css.md
+
+cd ../css3
+cat $(cat ../../../chpt-css3.list) > ../../../tmp/css3.md
 
 cd ../js
 cat $(cat ../../../chpt-js.list) > ../../../tmp/js.md
 
 
 cd ../../../tmp
-cat html.md css.md js.md > tips-and-tricks.md
+cat xhtml.md html5.md css.md css3.md js.md > tips-and-tricks.md
 
--- src/chapters/html/local-storage.md
+++
@@ -1,6 +0,0 @@
-
-## Local Storage
-
-exaplain difference between cookies and localStarage, localSession
-
-TODO using local storage or cookies
